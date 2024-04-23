67f527c1657c279d9c51222684fdda9307490432
joseeliasnsf <joseeliasnsf@outlook.com>
2024-04-23 14:27:02 -0300

    Atualização

---
+++ javascript/nome.js
@@ -0,0 +1,5 @@
+var nome = prompt("Digite seu nome: ");
+var x = "Olá";
+console.log("Variaveis em Javascript");
+console.log("Texto: " + x + ", tipo de dado: "+ typeof x);
+console.log("nome: " + nome)
